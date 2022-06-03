ef923d99cd1892ed43219059f31936600042e3a9
Priti <maurya.priti03201@gmail.com>
2022-06-03 23:35:54 +0530

    done

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+   
+    document.getElementById('result').innerHTML="If you deposit"
+    +principal+" ,\<br\>at an interest rate of "
+    +rate+"%\<br\>You will receive an amount of "
+    +interest+",\<br\>in the year "
+    +year+"\<br\>"
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+function validateForm(){
+    var principal = document.getElementById("principal").value;
+    if( document.getElementById("principal")=="")
+    {
+        alert("Enter Positive number");
+        principal.focus();
+    }
+    if(principal<=0 ) {
+        alert("Enter Positive number");
+        principal.focus();
+    }
+}
